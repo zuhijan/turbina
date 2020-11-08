aad7983b13944827fb55568d84aa8e8dc8b2d849
Egor <zuhijan@yandex.ru>
2020-11-08 22:44:35 +0600

    init project

---
+++ declaration.d.ts
@@ -0,0 +1,15 @@
+declare module '*.scss';
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './Main.module.scss';
+
+const Home = () => {
+  return <div className={s.root}>Главная</div>;
+};
+
+export default Home;
